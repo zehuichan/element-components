4646b95c972e69ad0c191cfb0ec6bf8619925b08
jasconchan <jasonchenzehui@gmail.com>
2021-04-18 21:56:03 +0800

    d

--- examples/App.vue
+++ examples/App.vue
@@ -1,19 +1,9 @@
 <template>
-  <div id="app">
-    <el-button @click="show = true">click</el-button>
-    <v-dialog v-model="show">
-      123
-    </v-dialog>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
   export default {
-    name: 'App',
-    data() {
-      return {
-        show: false
-      }
-    },
+    name: 'App'
   }
 </script>
