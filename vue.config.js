74b418c4bd18b623bd84b8d07f3cf6cdcf1f218e
chenzehui <jasonchenzehui@gmail.com>
2021-06-03 15:24:48 +0800

    fix：关闭gzip

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 'use strict'
 const path = require('path')
-const CompressionPlugin = require('compression-webpack-plugin')
 const isProd = process.env.NODE_ENV === 'production'
 
 function resolve(dir) {
@@ -20,16 +19,7 @@ module.exports = {
         'examples': resolve('examples'),
         'lib': resolve('lib'),
       }
-    },
-    plugins: isProd ? [
-      new CompressionPlugin({
-        algorithm: 'gzip',
-        test: /\.(js|css)(\?.*)?$/i,
-        threshold: 10240, // 对超过10k的数据进行压缩
-        minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
-        deleteOriginalAssets: false, // 删除原文件
-      })
-    ] : []
+    }
   },
   css: {
     extract: false
