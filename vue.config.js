7ceb9867a22714215873187bec116821eee6fbd0
jasconchan <jasonchenzehui@gmail.com>
2021-06-19 23:13:42 +0800

    feat: 支持markdown写法，以及.md文件引入到vue中使用

--- vue.config.js
+++ vue.config.js
@@ -21,6 +21,22 @@ module.exports = {
       }
     }
   },
+  chainWebpack(config) {
+    config.module
+      .rule('md')
+      .test(/\.md$/)
+      .use("vue-loader")
+      .loader("vue-loader")
+      .options({
+        compilerOptions: {
+          preserveWhitespace: false
+        }
+      })
+      .end()
+      .use('@vant/markdown-loader')
+      .loader(require.resolve('@vant/markdown-loader'))
+      .end()
+  },
   css: {
     extract: false
   }
