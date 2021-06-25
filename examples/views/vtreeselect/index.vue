359639e6ccc66065c889eff64eb216bb436ba0d9
jasconchan <jasonchenzehui@gmail.com>
2021-06-25 17:26:43 +0800

    feat: 新增vtreeselect组件

--- examples/views/vtreeselect/index.vue
+++ examples/views/vtreeselect/index.vue
@@ -1,53 +1,44 @@
 <template>
-  <demo-wrapper title="表单组件">
-    <!--v-badge-->
-    <demo-section>
-      <demo-card title="v-tree-select">
-        <v-tree-select v-model="value" :options="options"/>
-      </demo-card>
-      <demo-card style="width: 60%;">
-        <demo-block title="template">
-          <highlightjs language="html" :code="vtreeselect.template"/>
-        </demo-block>
-      </demo-card>
-    </demo-section>
+  <demo-wrapper title="v-tree-select 树选择">
+    <demo-card title="v-tree-select">
+      <v-tree-select v-model="form.value" :options="options" clearable/>
+    </demo-card>
+    {{ form }}
   </demo-wrapper>
 </template>
 
 <script>
-// code
-import { vtreeselect } from './code'
-
-export default {
-  name: 'vtreeselect',
-  data() {
-    return {
-      vtreeselect,
-      value: [],
-      options: [
-        {
-          id: 'a',
-          label: 'a',
-          children: [
-            {
-              id: 'aa',
-              label: 'aa',
-            }, {
-              id: 'ab',
-              label: 'ab',
-            }
-          ],
+  export default {
+    name: 'vtreeselect',
+    data() {
+      return {
+        form: {
+          value: null
         },
-        {
-          id: 'b',
-          label: 'b',
-        },
-        {
-          id: 'c',
-          label: 'c',
-        }
-      ],
+        options: [
+          {
+            id: 'a',
+            label: 'a',
+            children: [
+              {
+                id: 'aa',
+                label: 'aa',
+              }, {
+                id: 'ab',
+                label: 'ab',
+              }
+            ],
+          },
+          {
+            id: 'b',
+            label: 'b',
+          },
+          {
+            id: 'c',
+            label: 'c',
+          }
+        ],
+      }
     }
   }
-}
 </script>
--- lib/components/VTreeSelect/VTreeSelect.vue
+++ lib/components/VTreeSelect/VTreeSelect.vue
@@ -1,56 +1,84 @@
 <template>
-
+  <tree-select
+    v-bind="$attrs"
+    v-on="$listeners"
+    :value="value"
+    @input="input"
+  >
+    <template v-for="(val, name) of $slots" v-slot:[name]>
+      <slot :name="name"/>
+    </template>
+  </tree-select>
 </template>
 
 <script>
-export default {
-  name: 'VTreeSelect'
-}
+  import TreeSelect from '@riophae/vue-treeselect'
+  import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+
+  export default {
+    name: 'VTreeSelect',
+    model: {
+      prop: 'value',
+      event: 'update:value'
+    },
+    props: {
+      value: [String, Number]
+    },
+    methods: {
+      input(value, instanceId) {
+        this.$emit('update:value', value === undefined ? null : value)
+      }
+    },
+    components: {
+      TreeSelect
+    }
+  }
 </script>
 
-<style lang="script">
-.vue-treeselect {
-  line-height: 18px;
-  color: #606266;
-}
+<style lang="scss">
+  .vue-treeselect {
+    line-height: 18px;
+    color: #606266;
+    font-size: 13px;
+  }
 
-.vue-treeselect__control {
-  height: 32px;
-  border-radius: 4px;
-}
+  .vue-treeselect__control {
+    height: 32px;
+    border-radius: 4px;
+  }
 
-.vue-treeselect__input {
-  -webkit-appearance: none;
-  background-image: none;
-  border-radius: 4px;
-  display: block;
-  box-sizing: border-box;
-  color: #606266;
-  display: inline-block;
-  outline: none;
-}
+  .vue-treeselect__input {
+    -webkit-appearance: none;
+    background-image: none;
+    border-radius: 4px;
+    display: block;
+    box-sizing: border-box;
+    color: #606266;
+    display: inline-block;
+    outline: none;
+  }
 
-.vue-treeselect__label {
-  font-size: 14px;
-  padding: 0 20px;
-  position: relative;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  color: #606266;
-  height: 32px;
-  line-height: 32px;
-  box-sizing: border-box;
-  cursor: pointer;
-}
+  .vue-treeselect__label {
+    font-size: 14px;
+    padding: 0 20px;
+    position: relative;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    color: #606266;
+    height: 32px;
+    line-height: 32px;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
 
-.vue-treeselect__option--selected .vue-treeselect__label {
-  color: #409eff;
-}
+  .vue-treeselect__option--selected .vue-treeselect__label {
+    color: #409eff;
+  }
 
-.vue-treeselect__placeholder, .vue-treeselect__single-value {
-  padding-left: 10px;
-  padding-right: 10px;
-  line-height: 32px;
-}
+  .vue-treeselect__placeholder, .vue-treeselect__single-value {
+    padding-left: 10px;
+    padding-right: 10px;
+    line-height: 32px;
+  }
 </style>
