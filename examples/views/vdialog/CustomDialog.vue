8cd62184e3fb671150615e1370355a9783957684
jasconchan <jasonchenzehui@gmail.com>
2021-06-25 17:25:06 +0800

    feat: vdialog更新二次封装demo

--- examples/views/vdialog/CustomDialog.vue
+++ examples/views/vdialog/CustomDialog.vue
@@ -7,6 +7,7 @@
     @confirm="onConfirm"
     @cancel="onCancel"
   >
+    <v-form v-model="form" :options="options" label-position="left" label-width="80px"/>
   </v-dialog>
 </template>
 
@@ -14,7 +15,24 @@
   export default {
     data() {
       return {
-        loading: false
+        loading: false,
+        form: {},
+        options: [
+          { label: '角色名称', key: 'name', value: null, placeholder: '角色名称', type: 'input' },
+          { label: '角色值', key: 'val', value: null, placeholder: '角色值', type: 'input' },
+          {
+            label: '状态',
+            key: 'status',
+            value: null,
+            placeholder: '角色值',
+            type: 'switch',
+            options: [
+              { label: '启用', },
+              { label: '停用', }
+            ]
+          },
+          { label: '备注', key: 'remark', value: null, placeholder: '备注', type: 'textarea' },
+        ]
       }
     },
     methods: {
