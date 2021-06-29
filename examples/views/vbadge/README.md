a6646eb6f57c6aa8f2a808011510919b00ed47a8
chenzehui <jasonchenzehui@gmail.com>
2021-06-29 11:44:00 +0800

    feat: vbadge文档更新

---
+++ examples/views/vbadge/README.md
@@ -0,0 +1,36 @@
+### 介绍
+
+用于表示状态的小圆点。
+
+支持自定义具体的色值。
+
+### 基础用法
+
+```html
+<!--基础-->
+<v-badge/>
+<v-badge status="error"/>
+<v-badge status="default"/>
+<v-badge status="processing"/>
+<v-badge status="warning"/>
+<!--文案-->
+<v-badge>Success</v-badge>
+<v-badge status="error">Error</v-badge>
+<v-badge status="default">Default</v-badge>
+<v-badge status="processing">Processing</v-badge>
+<v-badge status="warning">Warning</v-badge>
+<!--自定义颜色-->
+<v-badge color="#f50">Custom #f50</v-badge>
+<v-badge color="#2db7f5">Custom #2db7f5</v-badge>
+<v-badge color="#87d068">Custom #87d068</v-badge>
+<v-badge color="#108ee9">Custom #108ee9</v-badge>
+```
+
+## API
+
+### Base Props
+
+| 参数   | 说明           | 类型      | 默认值 |
+| ------ | -------------- | --------- | ------ |
+| status | 类型 success / error / default / processing / warning | _string_  | `success`    |
+| color | 颜色值 | _string_  | _    |
--- examples/views/vbadge/index.vue
+++ examples/views/vbadge/index.vue
@@ -1,55 +1,41 @@
 <template>
-  <demo-wrapper title="展示组件">
-    <!--v-badge-->
-    <demo-section>
-      <demo-card title="v-badge">
-        <v-badge/>
-        <v-badge status="error"/>
-        <v-badge status="default"/>
-        <v-badge status="processing"/>
-        <v-badge status="warning"/>
-        <br/>
-        <v-badge>Success</v-badge>
-        <br/>
-        <v-badge status="error">Error</v-badge>
-        <br/>
-        <v-badge status="default">Default</v-badge>
-        <br/>
-        <v-badge status="processing">Processing</v-badge>
-        <br/>
-        <v-badge status="warning">Warning</v-badge>
-        <br/>
-        <v-badge color="#f50">Custom #f50</v-badge>
-        <br/>
-        <v-badge color="#2db7f5">Custom #2db7f5</v-badge>
-        <br/>
-        <v-badge color="#87d068">Custom #87d068</v-badge>
-        <br/>
-        <v-badge color="#108ee9">Custom #108ee9</v-badge>
-        <demo-block title="状态点">
-          <p>用于表示状态的小圆点。</p>
-          <p>支持自定义具体的色值。</p>
-        </demo-block>
-      </demo-card>
-      <demo-card style="width: 60%;">
-        <demo-block title="template">
-          <highlightjs language="html" :code="vbadge.template"/>
-        </demo-block>
-      </demo-card>
-    </demo-section>
+  <demo-wrapper title="v-badge 状态点">
+    <demo-card title="栗子">
+      <v-badge/>
+      <v-badge status="error"/>
+      <v-badge status="default"/>
+      <v-badge status="processing"/>
+      <v-badge status="warning"/>
+      <br/>
+      <v-badge>Success</v-badge>
+      <br/>
+      <v-badge status="error">Error</v-badge>
+      <br/>
+      <v-badge status="default">Default</v-badge>
+      <br/>
+      <v-badge status="processing">Processing</v-badge>
+      <br/>
+      <v-badge status="warning">Warning</v-badge>
+      <br/>
+      <v-badge color="#f50">Custom #f50</v-badge>
+      <br/>
+      <v-badge color="#2db7f5">Custom #2db7f5</v-badge>
+      <br/>
+      <v-badge color="#87d068">Custom #87d068</v-badge>
+      <br/>
+      <v-badge color="#108ee9">Custom #108ee9</v-badge>
+    </demo-card>
+    <README/>
   </demo-wrapper>
 </template>
 
 <script>
-// code
-import { vbadge } from './code'
+import README from './README.md'
 
 export default {
   name: 'vbadge',
-  data() {
-    return {
-      vbadge,
-    }
+  components: {
+    README
   }
 }
 </script>
