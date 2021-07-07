6615f8c601f22de9e32504b4bcc454b89183c0f2
chenzehui <jasonchenzehui@gmail.com>
2021-07-07 16:50:42 +0800

    feat: fragment改用jsx写法

--- lib/components/Fragment/Fragment.jsx
+++ lib/components/Fragment/Fragment.jsx
@@ -1,8 +1,9 @@
 // vue版Fragment
-export default {
+const Fragment = {
   name: 'Fragment',
-  functional: true,
-  render: (h, ctx) => {
+  render(h, ctx) {
     return ctx.children.length > 1 ? h('div', {}, ctx.children) : ctx.children
   }
 }
+
+export default Fragment
