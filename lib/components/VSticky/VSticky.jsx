d9607798257db53a4599c8a531fb7be60ce8f8f5
chenzehui <jasonchenzehui@gmail.com>
2021-07-09 17:25:14 +0800

    feat: add vsticky组件

---
+++ lib/components/VSticky/VSticky.jsx
@@ -0,0 +1,97 @@
+const VSticky = {
+  name: 'VSticky',
+  props: {
+    offsetTop: {
+      type: Number,
+      default: 0
+    },
+    zIndex: {
+      type: Number,
+      default: 10
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      active: false,
+      position: '',
+      width: undefined,
+      height: undefined,
+      isSticky: false
+    }
+  },
+  mounted() {
+    this.height = this.$el.getBoundingClientRect().height
+    window.addEventListener('scroll', this.handleScroll)
+    window.addEventListener('resize', this.handleResize)
+  },
+  activated() {
+    this.handleScroll()
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.handleScroll)
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    sticky() {
+      if (this.active) {
+        return
+      }
+      this.position = 'fixed'
+      this.active = true
+      this.width = this.width + 'px'
+      this.isSticky = true
+    },
+    handleReset() {
+      if (!this.active) {
+        return
+      }
+      this.reset()
+    },
+    reset() {
+      this.position = ''
+      this.width = 'auto'
+      this.active = false
+      this.isSticky = false
+    },
+    handleScroll() {
+      const width = this.$el.getBoundingClientRect().width
+      this.width = width || 'auto'
+      const offsetTop = this.$el.getBoundingClientRect().top
+      if (offsetTop < this.offsetTop) {
+        this.sticky()
+        return
+      }
+      this.handleReset()
+    },
+    handleResize() {
+      if (this.isSticky) {
+        this.width = this.$el.getBoundingClientRect().width + 'px'
+      }
+    }
+  },
+  render() {
+    const { height, zIndex, className, isSticky, offsetTop, position, width, $slots } = this
+    return (
+      <div style={{ height: height + 'px', zIndex: zIndex }}>
+        <div
+          class={className}
+          style={{
+            top: (isSticky ? offsetTop + 'px' : ''),
+            zIndex: zIndex,
+            position: position,
+            width: width,
+            height: height + 'px'
+          }}
+        >
+          {$slots.default}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default VSticky
