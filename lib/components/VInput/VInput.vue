8199f962a455d3183d7e6d0192d16f58fe963d59
jasconchan <jasonchenzehui@gmail.com>
2021-07-13 21:15:10 +0800

    fix: vinput初始化值后报错

--- lib/components/VInput/VInput.vue
+++ lib/components/VInput/VInput.vue
@@ -12,44 +12,44 @@
 </template>
 
 <script>
-import { formatNumber } from 'lib/utils/formate-number'
+  import { formatNumber } from 'lib/utils/formate-number'
 
-export default {
-  name: 'VInput',
-  model: {
-    prop: 'value',
-    event: 'input'
-  },
-  props: {
-    value: [String, Number]
-  },
-  data() {
-    return {
-      inputValue: this.value
-    }
-  },
-  computed: {
-    type() {
-      return this.$attrs.type === 'number' ? '_number' : this.$attrs.type
-    }
-  },
-  watch: {
-    value(val) {
-      this.inputValue = val
+  export default {
+    name: 'VInput',
+    model: {
+      prop: 'value',
+      event: 'input'
     },
-    inputValue(val) {
-      let value
-      switch (this.type) {
-        case 'digit':
-        case '_number':
-          const isNumber = this.type === '_number'
-          value = formatNumber(val, isNumber, isNumber)
-          break
-        default:
-          value = val
+    props: {
+      value: [String, Number]
+    },
+    data() {
+      return {
+        inputValue: this.value
       }
-      this.$emit('input', value)
+    },
+    computed: {
+      type() {
+        return this.$attrs.type === 'number' ? '_number' : this.$attrs.type
+      }
+    },
+    watch: {
+      value(val) {
+        this.inputValue = val
+      },
+      inputValue(val) {
+        let value
+        switch (this.type) {
+          case 'digit':
+          case '_number':
+            const isNumber = this.type === '_number'
+            value = formatNumber(val || '', isNumber, isNumber)
+            break
+          default:
+            value = val
+        }
+        this.$emit('input', value)
+      }
     }
   }
-}
 </script>
