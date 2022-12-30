b01aec8e72683870c1fc9b40cfff806cb5b1b6b7
Zehui Chan <jasonchenzehui@gmail.com>
2022-12-30 17:50:35 +0800

    feat: VSelect支持自定义模板

--- examples/views/vselect/index.vue
+++ examples/views/vselect/index.vue
@@ -1,9 +1,14 @@
 <template>
   <demo-wrapper title="VSelect 选择框">
-    <demo-card>
-      <v-select v-model="value" :options="options" clearable/>
+    <demo-card title="基础用法">
+      <v-select v-model="value" :options="options" clearable />
     </demo-card>
-    <README/>
+    <demo-card title="自定义模板">
+      <v-select v-model="value" :options="options" clearable>
+        <template #default="scope">{{ scope }}</template>
+      </v-select>
+    </demo-card>
+    <README />
   </demo-wrapper>
 </template>
 
@@ -32,7 +37,7 @@ export default {
           value: '45HQ',
           label: '45HQ'
         }
-      ],
+      ]
     }
   },
   components: {
